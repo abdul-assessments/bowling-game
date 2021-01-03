d781f8bffb2f038a7652fdd17faf1114194fc484
Abdul Gool <abdul-wahaab.gool@ogilvy.co.za>
2021-01-03 20:53:34 +0200

    UX improvements

--- BowlingGame.Web/my-app/src/app/app.component.ts
+++ BowlingGame.Web/my-app/src/app/app.component.ts
@@ -7,7 +7,8 @@ import { BowlingGameService } from './bowling-game.service';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Bowling Game';
+  title = 'Welcome to the Imaginary Bowling Game';
+  description = 'Where it literally all happens in your head, the app just keeps score.';
 
   constructor(public bowlingGameService: BowlingGameService) {
   }
